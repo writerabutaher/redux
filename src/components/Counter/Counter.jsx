42f0da2097ccdfde95fc0a8a292b262994dfd9b6
Abu Taher <writerabutaher@gmail.com>
2023-11-18 18:24:24 +0600

    counter UI

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,19 +1,17 @@
-"use client";
-
-import { useState } from "react";
-import CounterButton from "./CounterButton";
-
 const Counter = () => {
-  const [count, setCount] = useState(0);
   return (
     <div className="grid place-items-center h-screen">
       <div className="bg-sky-500 rounded p-16 flex flex-col items-center gap-8">
         <div className="px-4 py-2 bg-pink-500 rounded">
-          <h1 className="text-4xl text-white">{count}</h1>
+          <h1 className="text-4xl text-white">{value}</h1>
         </div>
         <div className="space-x-4">
-          <CounterButton setCount={setCount}>Increment</CounterButton>
-          <CounterButton setCount={setCount}>Decrement</CounterButton>
+          <button className="text-2xl py-2 px-4 text-white bg-pink-500 rounded">
+            Increment
+          </button>
+          <button className="text-2xl py-2 px-4 text-white bg-pink-500 rounded">
+            Decrement
+          </button>
         </div>
       </div>
     </div>
