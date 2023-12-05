6afd4738f83ea8d8524b99336a775165f0a45988
Abu Taher <writerabutaher@gmail.com>
2023-12-05 16:39:10 +0600

    use dispatch

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,12 +1,24 @@
 "use client";
 
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   actionIncrement,
   actionDecrement,
 } from "@/components/redux/counter/actions";
 
-const Counter = ({ count, increment, decrement }) => {
+const Counter = () => {
+  const count = useSelector((state) => state.value);
+
+  const dispatch = useDispatch();
+
+  const incrementHandler = (value) => {
+    dispatch(actionIncrement(value));
+  };
+
+  const decrementHandler = (value) => {
+    dispatch(actionDecrement(value));
+  };
+
   return (
     <div className="grid h-screen place-items-center">
       <div className="flex flex-col items-center gap-8 p-16 rounded bg-sky-500">
@@ -15,13 +27,13 @@ const Counter = ({ count, increment, decrement }) => {
         </div>
         <div className="space-x-4">
           <button
-            onClick={increment}
+            onClick={() => incrementHandler()}
             className="px-4 py-2 text-2xl text-white bg-pink-500 rounded"
           >
             Increment
           </button>
           <button
-            onClick={decrement}
+            onClick={() => decrementHandler()}
             className="px-4 py-2 text-2xl text-white bg-pink-500 rounded"
           >
             Decrement
@@ -32,17 +44,4 @@ const Counter = ({ count, increment, decrement }) => {
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    count: state.value,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    increment: (value) => dispatch(actionIncrement(value)),
-    decrement: (value) => dispatch(actionDecrement(value)),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default Counter;
