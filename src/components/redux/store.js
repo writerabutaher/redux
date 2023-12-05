04867a1da279d2c613a700c5db5aeedf5d4c8720
Abu Taher <writerabutaher@gmail.com>
2023-12-05 20:57:47 +0600

    redux logger

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,4 +1,9 @@
-import { createStore } from "@reduxjs/toolkit";
+import { applyMiddleware, createStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-export const store = createStore(rootReducer);
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
